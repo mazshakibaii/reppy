6ccd96e8a4908b744780d3804b73a516b514903d
mike dupont <mike.dupont@introspector.local>
2025-01-10 13:52:34 -0500

    udpate model

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -176,7 +176,9 @@ const defaultModels = {
   //cohere: "command",
  //mistral: "mistral-tiny",
 //    bedrock: "claude-3.5-sonnet",
-    bedrock:"meta.llama3-3-70b-instruct-v1:0",
+    bedrock:    "us.anthropic.claude-3-sonnet-20240229-v1:0us.anthropic.claude-3-sonnet-20240229-v1:0",
+    //"meta.llama3-3-70b-instruct-v1:0",
+
     //"meta.llama3-70b-instruct-v1:0",
     
   //groq: "mixtral-8x7b-32768",
